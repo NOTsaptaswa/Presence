9e0a44e56aaae74c580b6d8f12dc968d312ed966
NOTsaptaswa <nandi.saptaswa@gmail.com>
2026-05-09 13:27:25 +0530

    Initial Commit

---
+++ PresenceTests/PresenceTests.swift
@@ -0,0 +1,17 @@
+//
+//  PresenceTests.swift
+//  PresenceTests
+//
+//  Created by Saptaswa Nandi on 09/05/26.
+//
+
+import Testing
+@testable import Presence
+
+struct PresenceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
